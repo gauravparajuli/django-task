0eebc4bf015dfcd2299908e1c00eb413969115aa
gaurav parajuli <gparajuli.1997@gmail.com>
2023-04-14 17:07:07 +0545

    created employee app

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    firstname = models.CharField(max_length=255, null=False)
+    lastname = models.CharField(max_length=255, null=False)
+    dob = models.DateTimeField(null=False)
+    salary = models.IntegerField(null=False)
