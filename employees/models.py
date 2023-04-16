3c4ed27b11db5ecfdc0efed91da0d909cbdcb2cb
gaurav parajuli <gparajuli.1997@gmail.com>
2023-04-16 12:32:35 +0545

    implemented current_age method for Employee Model

--- employees/models.py
+++ employees/models.py
@@ -1,6 +1,8 @@
+from datetime import date, timedelta
 from django.db import models
+import math
 
-# Create your models here.
+# Create your models here
 
 
 class Employee(models.Model):
@@ -8,3 +10,10 @@ class Employee(models.Model):
     lastname = models.CharField(max_length=255, null=False)
     dob = models.DateField(null=False)
     salary = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f'{self.firstname} {self.lastname}'
+
+    def current_age(self):
+        age = date.today() - self.dob
+        return (age//timedelta(365.25))
